7c14a979cc4afe1d5269b7bc2c8131345678f80b
DA\nwuser <wakacaptain@gmail.com>
2019-06-14 11:30:15 +0900

    Project作成

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        PlayerRun();
+    }
+
+   private void  PlayerRun(){
+        Transform myTransform = this.transform;
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.W)){
+            pos.y += 0.05f; 
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= 0.05f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y -= 0.05f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += 0.05f;
+        }
+        myTransform.position = pos;
+    }
+}
