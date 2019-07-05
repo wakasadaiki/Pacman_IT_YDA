e4084c4b50e6b60372dc2b915c834de8b82bff96
DA\nwuser <wakacaptain@gmail.com>
2019-07-05 09:30:39 +0900

    Enemy挙動更新

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    int[] E_Move = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+    int[] E_Move = new int[] { 0, 1, 2, 3, 4};
+    int[] data = new int[] {1,2,3,4};
+    int x = 0;
+    int y = 0;
+    int i = 0;
+    int fps = 0;
 
     // Start is called before the first frame update
     void Start(){
@@ -12,8 +17,21 @@ public class Enemy : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() { 
-   
+    void Update() {
+        fps++;
+        if (fps > 100)
+        {
+            {
+                i++;
+                if (i > 3) i = 0;
+                EnemyRun();
+            }
+            fps = 0;
+        }
+
+        
+       
+        EnemyMove();
     }
 
     private void EnemyRun()
@@ -22,9 +40,44 @@ public class Enemy : MonoBehaviour
 
         Vector3 pos = myTransform.position;
 
+        pos.x += x;
+        pos.y += y;
+
         myTransform.position = pos; 
     }
 
+
     private void EnemyMove(){
+
+      
+            switch (data[i])
+            {
+                case 0:
+                    x = 0;
+                    y = 0;
+                    break;
+                case 1:
+                    x = 0;
+                    y = 1;
+                    break;
+                case 2:
+                    x = 1;
+                    y = 0;
+                    break;
+                case 3:
+                    x = 0;
+                    y = -1;
+                    break;
+                case 4:
+                    x = -1;
+                    y = 0;
+                    break;
+                default:
+                    Debug.Log("想定してないエラーは出れtl駒曽田");
+                    break;
+
+            }
+        
+
     }
 }
