3d3785f848ac6438843700fbbdbb3a4c44b5e608
superpoiful <kawashi.yda@gmail.com>
2019-07-05 11:47:01 +0900

    マップの当たり判定追加

---
+++ Assets/Script/cokkieGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C : MonoBehaviour
+{
+    public GameObject cookiePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
